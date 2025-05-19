ab786c89c2b4366c7d20adb3750b6f981d9fe765
orestulis05 <orestaspetkus@gmail.com>
2025-05-19 11:43:17 +0300

    .env reading & db connection

---
+++ App/Core/database.php
@@ -0,0 +1,25 @@
+<?php
+include_once("../Utils/dotenv.php");
+
+$db_host = get_dotenv_value("MYSQL_DB_HOST");
+$db_user = get_dotenv_value("MYSQL_DB_USERNAME");
+$db_pass = get_dotenv_value("MYSQL_DB_PASSWORD");
+$db_name = get_dotenv_value("MYSQL_DB_NAME");
+
+if (is_bool($db_host) || is_bool($db_user) || is_bool($db_name)) {
+  echo "<p>db env vars are not set up correctly.</p>";
+  exit;
+}
+
+// if no pass was set in .env file, default it to empty string
+if (is_bool($db_pass)) {
+  $db_pass = "";
+}
+
+try {
+  if (!$db_conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name)) {
+    die("db connection failed");
+  }
+} catch (mysqli_sql_exception) {
+  echo "<p>db connection failed.</p>";
+}
